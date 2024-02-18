fc121187498cc45f04bcfefb5c9c4323e79df335
xuanyu tang <105061290+ukmojb@users.noreply.github.com>
2024-02-18 22:06:32 +0800

    修复匠魂电力装载被爆头扣耐久

---
+++ iblis_headshots/event/IblisHeadshotsEventHandler.java
@@ -0,0 +1,150 @@
+package iblis_headshots.event;
+
+import c4.conarm.common.ConstructsRegistry;
+import c4.conarm.common.armor.utils.ArmorHelper;
+import c4.conarm.lib.tinkering.TinkersArmor;
+import com.google.common.collect.HashMultimap;
+
+import iblis_headshots.IblisHeadshotsMod;
+import iblis_headshots.advacements.criterion.HeadshotTrigger;
+import iblis_headshots.util.HeadShotHandler;
+import iblis_headshots.util.IblisItemUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import slimeknights.tconstruct.library.utils.TinkerUtil;
+
+public class IblisHeadshotsEventHandler {
+
+	public static float nonProjectileHeadshotMinDistanceSq = 16.0f;
+	public static float damageToItemMultiplier = 4.0f;
+	public static float damageMultiplier = 4.0f;
+	public static float missMultiplier = 1.0f;
+	public static boolean playersHaveNoHeads = false;
+	private boolean debug = false;
+	private int lastHandledEntityId = -1;
+
+	@SubscribeEvent
+	public void onLivingHurt(LivingHurtEvent event) {
+		if(event.isCanceled())
+			return;
+		float damage = event.getAmount();
+		EntityLivingBase victim = event.getEntityLiving();
+		lastHandledEntityId = victim.getEntityId();
+		event.setAmount(recalculateDamage(damage, victim, event.getSource()));
+	}
+	
+	@SubscribeEvent
+	public void onLivingDamage(LivingDamageEvent event) {
+		float damage = event.getAmount();
+		EntityLivingBase victim = event.getEntityLiving();
+		// Techguns2 ignore LivingHurtEvent, but using LivingDamageEvent. To avoid
+		// applying headshot twice, we need that check:
+		if (lastHandledEntityId == victim.getEntityId()) {
+			lastHandledEntityId = -1;
+			return;
+		}
+		event.setAmount(recalculateDamage(damage, victim, event.getSource()));
+	}
+
+	public float recalculateDamage(float damage, EntityLivingBase victim, DamageSource source) {
+		World world = victim.world;
+		if (world.isRemote || damage < 0.1f || !(world instanceof WorldServer))
+			return damage;
+		Entity projectile = source.getImmediateSource();
+		if (projectile == null)
+			return damage;
+		Vec3d start = new Vec3d(projectile.posX - projectile.motionX, projectile.posY - projectile.motionY,
+				projectile.posZ - projectile.motionZ);
+		Vec3d end = new Vec3d(projectile.posX + projectile.motionX, projectile.posY + projectile.motionY,
+				projectile.posZ + projectile.motionZ);
+		if (projectile instanceof EntityPlayer) {
+			double d = projectile.getDistance(victim);
+			if (d < nonProjectileHeadshotMinDistanceSq)
+				return damage;
+			start = new Vec3d(projectile.posX, projectile.posY + projectile.getEyeHeight(), projectile.posZ);
+			Vec3d aim = projectile.getLookVec();
+			end = new Vec3d(projectile.posX + aim.x * d, projectile.posY + projectile.getEyeHeight() + aim.y * d,
+					projectile.posZ + aim.z * d);
+		}
+		if (debug) {
+			for (int i = 20; i < 60; i++) {
+				Vec3d iv = start.add(projectile.motionX * i * 0.05, projectile.motionY * i * 0.05,
+						projectile.motionZ * i * 0.05);
+				IblisHeadshotsMod.network.spawnHeadshotParticle(victim.world, iv, Vec3d.ZERO, 150);
+			}
+		}
+		if ((!playersHaveNoHeads || !(victim instanceof EntityPlayer))
+				&& HeadShotHandler.traceHeadShot(victim, start, end) != null) {
+			IblisHeadshotsMod.network.spawnHeadshotParticle(victim.world,
+					new Vec3d(victim.posX, victim.posY + victim.getEyeHeight(), victim.posZ), new Vec3d(0d, 0.2d, 0d),
+					15);
+			float multiplier = damageMultiplier;
+			ItemStack headgear = victim.getItemStackFromSlot(EntityEquipmentSlot.HEAD);
+			HashMultimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
+			this.addModifierOfStackInSlot(victim, multimap, EntityEquipmentSlot.CHEST);
+			this.addModifierOfStackInSlot(victim, multimap, EntityEquipmentSlot.LEGS);
+			this.addModifierOfStackInSlot(victim, multimap, EntityEquipmentSlot.FEET);
+			if (!multimap.isEmpty()) {
+				victim.getAttributeMap().removeAttributeModifiers(multimap);
+				WorldServer wserver = (WorldServer) world;
+				wserver.addScheduledTask(() -> {
+					HashMultimap<String, AttributeModifier> multimap2 = HashMultimap
+							.<String, AttributeModifier>create();
+					this.addModifierOfStackInSlot(victim, multimap2, EntityEquipmentSlot.CHEST);
+					this.addModifierOfStackInSlot(victim, multimap2, EntityEquipmentSlot.LEGS);
+					this.addModifierOfStackInSlot(victim, multimap2, EntityEquipmentSlot.FEET);
+					victim.getAttributeMap().applyAttributeModifiers(multimap2);
+				});
+			}
+			if (!headgear.isEmpty()) {
+				float headGearDamageAbsorbMultiplier = IblisItemUtils.getHeadgearProtection(headgear);
+				multiplier = 1.0f + Math.max(multiplier - 1.0f, 0.0f) * headGearDamageAbsorbMultiplier;
+				if (Loader.isModLoaded("tconstruct")) {
+					if (headgear.getItem().getRegistryName() == ConstructsRegistry.helmet.getRegistryName() && victim instanceof EntityPlayer) {
+						EntityPlayer player = (EntityPlayer) victim;
+						ArmorHelper.damageArmor(headgear, source, (int) ((victim.world.rand.nextFloat()*0.5 + 1.0F) * damage * damageToItemMultiplier), player);
+					} else {
+						headgear.damageItem((int) ((victim.world.rand.nextFloat()*0.5 + 1.0F) * damage * damageToItemMultiplier), victim);
+					}
+				} else {
+					headgear.damageItem((int) ((victim.world.rand.nextFloat()*0.5 + 1.0F) * damage * damageToItemMultiplier), victim);
+				}
+			}
+			damage *= multiplier;
+			Entity shooter = source.getTrueSource();
+			if (shooter instanceof EntityPlayerMP && !(victim instanceof EntityPlayerMP)) {
+				HeadshotTrigger.instance.trigger((EntityPlayerMP) shooter, victim);
+			} else if (victim instanceof EntityPlayerMP) {
+				HeadshotTrigger.instance.trigger((EntityPlayerMP) victim, victim);
+			}
+			if (victim.getHealth() < damage && victim instanceof EntitySlime
+					&& ((EntitySlime) victim).getSlimeSize() > 1) {
+				((EntitySlime) victim).setSlimeSize(1, false);
+			}
+		} else {
+			damage *= missMultiplier;
+		}
+		return damage;
+	}
+
+	private void addModifierOfStackInSlot(EntityLivingBase entity, HashMultimap<String, AttributeModifier> multimap,
+			EntityEquipmentSlot slot) {
+		ItemStack stack = entity.getItemStackFromSlot(slot);
+		if (!stack.isEmpty())
+			multimap.putAll(stack.getAttributeModifiers(slot));
+	}
+}
